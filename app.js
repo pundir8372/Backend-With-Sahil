34ca419e605edb9f802c76349f5180c3ef7d8c76
pundir8372 <pundirsahil320@gmail.com>
2026-01-16 14:28:54 +0530

    Made some changes in the cde as per in the workflows

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-const express=require("express");
-const app=express();
-require('dotenv').config();
+const express = require("express");
+const app = express();
+require("dotenv").config();
 
-const port=4000;
 
-app.get("/", (req,res)=>{
+const PORT_VALUE = process.env.PORT_VALUE || 4000;
+
+app.get("/", (req, res) => {
   res.send("Hello World");
-})
-app.get("/twitter" , (req,res)=>{
+});
+
+app.get("/twitter", (req, res) => {
   res.send("Sahil Pundir");
-})
+});
 
-app.get("/login",(req,res)=>{
-  res.send('<h1>Welcome to login Page</h1>')
-})
+app.get("/login", (req, res) => {
+  res.send("<h1>Welcome to login Page</h1>");
+});
 
-app.listen(process.env.port,()=>{
-  console.log(`App Listening on port : ${process.env.port}`)
-})
+
+app.listen(PORT_VALUE, "0.0.0.0", () => {
+  console.log(`App Listening on port: ${PORT_VALUE}`);
+});
